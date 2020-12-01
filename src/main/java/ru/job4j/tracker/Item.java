49a98790333cd0ebc16e5d4c801b634c873b22ba
SlartiBartFast-art <JekHamond@tutanota.com>
2020-12-01 19:09:49 +0500

    6. Date. Отображение даты. [#392241]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,39 @@
 package ru.job4j.tracker;
 
+import org.w3c.dom.ls.LSOutput;
+
+
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
+    public LocalDateTime getCreated() {
+        return created;
+    }
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+
 }
+
+
+
+
+
+

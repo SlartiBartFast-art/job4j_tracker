07a39b9cee21c7465591ae55c15c2295c9ee56f2
SlartiBartFast-art <JekHamond@tutanota.com>
2020-12-02 00:10:56 +0500

    2. Перегрузка конструктора [#392244]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -9,6 +9,18 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
+    public Item() {
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -16,15 +28,18 @@ public class Item {
         this.id = id;
     }
     public String getName() {
+
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
     public LocalDateTime getCreated() {
+
         return created;
     }
     public void setCreated(LocalDateTime created) {
+
         this.created = created;
     }
 }

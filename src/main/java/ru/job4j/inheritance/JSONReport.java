9029a7d0d86fe94e15b512ec5a0951f5969e29ab
SlartiBartFast-art <JekHamond@tutanota.com>
2020-12-02 17:27:16 +0500

    4. Переопределение [#392245]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,14 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "name" + " : "  + name + System.lineSeparator() + "body" + " : " + body;
+    }
+
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("name1", "body1");
+        System.out.println(text);
+    }
+}

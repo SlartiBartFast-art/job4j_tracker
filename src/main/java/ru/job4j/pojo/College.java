83f93c2b2ce812d8f255b8cc45bb1ff0982200f7
SlartiBartFast-art <JekHamond@tutanota.com>
2020-12-02 22:44:49 +0500

    3.2. Модель данных. [#392252]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFioId("Ivanov Ivan");
+        student.setGroup(358);
+        student.setReceiptDate("02.12.2020");
+        System.out.println("FIO: " + student.getFioId() + " Group №: " + student.getGroup() + " Date reception: " + student.getReceiptDate());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fioId;
+    private int group;
+    private String receiptDate;
+
+    public String getFioId() {
+        return fioId;
+    }
+
+    public void setFioId(String fioId) {
+        this.fioId = fioId;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public String getReceiptDate() {
+        return receiptDate;
+    }
+
+    public void setReceiptDate(String receiptDate) {
+        this.receiptDate = receiptDate;
+    }
+}

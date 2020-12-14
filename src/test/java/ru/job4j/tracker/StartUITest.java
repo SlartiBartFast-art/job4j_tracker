96302332946c36b9355d82241e0c7326cf4cbb06
SlartiBartFast-art <JekHamond@tutanota.com>
2020-12-14 18:16:35 +0500

    9. Написать тесты на StartUI. [#392193]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,46 @@
 package ru.job4j.tracker;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
 
 public class StartUITest {
+
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(new String[] {"0", "Item name", "1"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new CreateAction(), new CreateExit()};
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        /* Добавим в tracker новую заявку */
+        Item item = tracker.add(new Item("Replaced item"));
+        /* Входные данные должны содержать ID добавленной заявки item.getId() */
+        String id = "1";
+        //int id = item.getId();
+         String replacedName = "New item name";
+        Input in = new StubInput(new String[] {"0", "1", "New item name", "1"}); /* входные параметры для ReplaceAction */
+        UserAction[] actions = {new CreateEditItem(), new CreateExit()};
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        /* Добавим в tracker новую заявку */
+        Item item = tracker.add(new Item("Deleted item"));
+        String id = "1";
+        /* Входные данные должны содержать ID добавленной заявки item.getId() */
+        Input in = new StubInput(new String[] {"0", "1", "1"});
+        UserAction[] actions = {new CreateDeleteItem(), new CreateExit()};
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
 
     /*@Test
     public void whenAddItem() {

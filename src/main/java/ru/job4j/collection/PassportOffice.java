e00f1c204961bede032b000ed7a7e6a16a04f1cc
SlartiBartFast-art <JekHamond@tutanota.com>
2021-01-04 20:12:57 +0500

    1. Паспорт и жители [#392168]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**Сервис должен проверять, что гражданина с таким паспортом еще нет,
+ *  а так же возможность получить информацию о гражданине по его паспорту.
+ * */
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+       // String key = citizen.getPassport();
+
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            return true;
+        }
+        return rsl;
+    }
+
+
+    public Citizen get(String passport) {
+
+        for (String namekey : citizens.keySet()) {
+            if (namekey.equals(passport)) {
+                return citizens.get(namekey);
+            }
+        }
+        return null;
+    }
+}

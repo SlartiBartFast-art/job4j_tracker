d24c1fcd94a0340f8d8e6a8fdfcb742c50a30085
SlartiBartFast-art <JekHamond@tutanota.com>
2021-01-11 14:58:10 +0500

    3. Что такое JavaDoc [#392146]144]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,26 +5,53 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает:
+ * Регистрацию пользователя.
+ * Удаление пользователя из системы.
+ * Добавление пользователю банковского счета.
+ * Перевод денег с одного банковского счета на другой счет.
+ * @author DIMA KAZUMU
+ * @version 1.0
+ */
 public class BankService {
+
+    /**
+     * Это поле содержит всех пользователей системы с привязанными к ним счетами.
+     * Хранение задания осуществляется в коллекции типа List;
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод позволяет добавить пользователя в систему.
+     * @param user Пользователь банковского счета
+     * {@link ru.job4j.bank.User}
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод производит добавление нового счета к пользователю
+     * @param passport паспортные данные пользователя
+     * @param account новый счет пользователя
+     */
+
     public void addAccount(String passport, Account account) {
-        User user1 = findByPassport(passport); // нашли по паспорту
+        User user1 = findByPassport(passport);
         if (user1 != null) {
             List<Account> accounts = users.get(user1);
             if (!accounts.contains(account)) {
-            accounts.add(account);
+                accounts.add(account);
             }
-
         }
-
     }
 
-/**найти пользователя по паспорту*/
+    /**
+     * Метод ищет пользователя по номеру паспорт
+     * @param passport паспортные данные пользователя
+     * @return возвращает найденного пользователя или null если такого пользователя нет
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
 
@@ -34,14 +61,17 @@ public class BankService {
         }
         return null;
     }
-/**Этот метод идет счет пользователя по реквизитам. Сначала нужно найти пользователя.
- *  Потом получить список счетов этого пользователя и в нем найти нужный счет.
- * */
 
+    /**
+     * Метод осуществляет поиск счет пользователя по реквизитам.
+     * @param passport паспортные данные пользователя
+     * @param requisite реквизиты счта пользователя
+     * @return
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user1 = findByPassport(passport);
         if (user1 != null) {
-                List<Account> accounts = users.get(user1); //получили список счетов
+            List<Account> accounts = users.get(user1); //получили список счетов
             for (int i = 0; i < accounts.size(); i++) {
                 if (requisite.equals(accounts.get(i).getRequisite())) {
                     return accounts.get(i);
@@ -50,8 +80,17 @@ public class BankService {
         }
         return null;
     }
-/**Метод для перечисления денег с одного счёта на другой счёт.
- * */
+
+    /**
+     * Метод позволяет осуществлять перечисления денег с одного счёта на другой счёт.
+     * @param srcPassport паспортные данные пользователя счета с которго осуществляется перевод
+     * @param srcRequisite реквизиты счета пользователя с которго осуществляется перевод
+     * @param destPassport паспортные данные пользователя счета на которго осуществляется перевод
+     * @param destRequisite реквизиты счета пользователя на который осуществляется перевод
+     * @param amount сумма баланса счета
+     * @return true перечисление было выполнено успешно.
+     *         false если перечисление не было произведено.
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite, // откуда
                                  String destPassport, String destRequisite, double amount) { // куда
@@ -64,6 +103,6 @@ public class BankService {
             rsl = true;
         }
         return rsl;
-        }
     }
+}
 
--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,34 +1,46 @@
 package ru.job4j.search;
 
 import java.util.LinkedList;
+
+/**
+ * Класс описывает работу простейшей очереди по приоритету, которая работает
+ * по принципу FIFO (first in - first out) *
+ * @author DIMA KAZUMU
+ * @version 1.0
+ */
 public class PriorityQueue {
+
+    /**
+     * Хранение задание осуществляется в коллекции типа LinkedList
+     */
     private LinkedList<Task> tasks = new LinkedList<>();
 
-    /**напишем каркас очереди с приоритетами.
-     * В методе put вам нужно найти индекс нужного элемента.-кому нужно-для чего нужного?
-     * Метод должен вставлять в нужную позицию элемент.
-     * Позиция определяется по полю приоритет.
-     * Для вставки использовать add(int index, E value)
-     * @param task задача
-     * Task task = new Task(String desc, int priority); /список задач и день с указанием их важности
+    /**
+     * Метод принимает на вход заявку и добавляет ее в очередь.
+     * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
+     * они распределяются по принципу FIFO.
      *
-     *    task.add(int priority, String desc, );
+     * @param task задача которая добавляется в очередь
+     *             Task task = new Task(String desc, int priority); /список задач и день с указанием их важности
+     *             <p>
+     *             task.add(int priority, String desc, );
      */
     public void put(Task task) {
         int index = 0;
+        for (Task element : tasks) {
+            if (element.getPriority() > task.getPriority()) {
 
-            for (Task element : tasks) {
-                if (element.getPriority() > task.getPriority()) {
-
-                    break;
-                }
-                index++;
-
+                break;
             }
-
+            index++;
+        }
         this.tasks.add(index, task);
     }
 
+    /**
+     * Метод позволяет получить первую задачу в очереди
+     * @return возвращает задачу из головы очереди или null если очередь пуста
+     */
     public Task take() {
         return tasks.poll();
     }

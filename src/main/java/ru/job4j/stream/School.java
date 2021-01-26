233c30cef813f7cf12d67beb7a8d83778f1e2673
SlartiBartFast-art <JekHamond@tutanota.com>
2021-01-26 14:08:45 +0500

    1. Фильтрация учеников [#391992]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,17 +1,19 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class School {
-    public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> students1 = students.stream().filter(predict)
-                .collect(Collectors.toList());
+    public static List<Student> collect(List<Student> students,
+                                        Predicate<Student> predict) {
 
-        return students1;
+        return students.stream().filter(predict)
+                .collect(Collectors.toList());
     }
 
+    /*List<Student> students1 = students.stream().filter(predict)
+   было             .collect(Collectors.toList());
     Predicate<Student> predict = student -> student.getScore() > 50;
+     */
 }
